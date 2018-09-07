19d9985f5f323d9aecf2558b3843bc333a44fa7b
vthiery <vjmthiery@gmail.com>
2018-09-07 14:49:18 +0200

    Move the ECB holidays in a dedicated file

--- holiday_defs.go
+++ holiday_defs.go
@@ -15,28 +15,6 @@ var (
 	Christmas2   = NewHoliday(time.December, 26)
 )
 
-// European Central Bank Target2 holidays
-var (
-	ECBGoodFriday       = GoodFriday
-	ECBEasterMonday     = EasterMonday
-	ECBNewYearsDay      = NewYear
-	ECBLabourDay        = NewHoliday(time.May, 1)
-	ECBChristmasDay     = Christmas
-	ECBChristmasHoliday = Christmas2
-)
-
-// AddEcbHolidays adds all ECB Target2 holidays to the calendar
-func AddEcbHolidays(c *Calendar) {
-	c.AddHoliday(
-		ECBGoodFriday,
-		ECBEasterMonday,
-		ECBNewYearsDay,
-		ECBLabourDay,
-		ECBChristmasDay,
-		ECBChristmasHoliday,
-	)
-}
-
 func calculateEaster(year int, loc *time.Location) time.Time {
 	// Meeus/Jones/Butcher algorithm
 	y := year
