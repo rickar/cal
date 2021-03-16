78de12ff6280ce4befca6380009ec1761ea1d86e
Hyungsuk Hong <aanoaa@gmail.com>
2021-03-17 00:30:25 +0900

    Add kr.Lunar2Solar

--- v2/kr/lunar.go
+++ v2/kr/lunar.go
@@ -7,6 +7,8 @@ import (
     "log"
     "sort"
     "time"
+
+    "github.com/rickar/cal/v2"
 )
 
 const (
@@ -17,15 +19,10 @@ const (
     MAXYEAR = 2100
 )
 
-// Lun2Sol converts to solar calendar time
-// TODO: implement if needed
-// func Lun2Sol(year, month, day int) time.Time {
-//     return time.Now()
-// }
-
 // Sol2Lun converts to lunar year/month/day and if it is belongs to the leap month
 // Chinese lunar calendar
 func Sol2Lun(t time.Time) (ymd [3]int, isLeap bool) {
+
     offset := offsetDays(MINDATE, t)
     year, offset := lunarYearByOffset(offset)
     leapMonth := leapMonth(year)
@@ -181,6 +178,54 @@ func Solar2Lunar(t time.Time) (ymd [3]int, isLeap bool) {
     return ymd, isLeap
 }
 
+// Lunar2Solar converts to solar calendar time
+func Lunar2Solar(year, month, day int, isLeap bool) time.Time {
+    const (
+        BASEYEAR = 1391
+        MINDATE  = 2229156 // 1391-02-05 ( lunisolar 1391-01-01 )
+        MAXDATE  = 2470172 // 2050-12-31 ( lunisolar 2050-11-18 )
+    )
+
+    year -= BASEYEAR
+    if year < 0 || year >= len(YearTable) {
+        log.Fatal("Year is out of range")
+    }
+
+    if month < 1 || month > 12 {
+        log.Fatal("Month is out of range")
+    }
+
+    if isLeap && LeapTable[year]-1 != month {
+        log.Fatal("Wrong leap month")
+    }
+
+    months := YearTable[year] + month - 1
+    if isLeap && month+1 == LeapTable[year] {
+        months++
+    } else if LeapTable[year] != 0 && LeapTable[year] < month {
+        months++
+    }
+
+    days := MonthTable[months] + day - 1
+    if day < 1 || days >= MonthTable[months+1] {
+        log.Fatal("Wrong day")
+    }
+
+    y, m, d := julianDayToDate(days + MINDATE)
+    return time.Date(y, time.Month(m), d, 0, 0, 0, 0, cal.DefaultLoc)
+}
+
+func julianDayToDate(jd int) (year, month, day int) {
+    if jd >= 0 {
+        if jd >= 2299161 {
+            year, month, day = julianDayToGregorianDate(jd)
+        } else {
+            year, month, day = julianDayToJulianDate(jd)
+        }
+    }
+    return
+}
+
 // julian https://github.com/toelsiba/date/blob/master/julian_day.go
 // Gregorian calendar starting from October 15, 1582
 // Algorithm from Henry F. Fliegel and Thomas C. Van Flandern
@@ -191,4 +236,36 @@ func julian(t time.Time) (jd int) {
     e += -(3 * ((year + 4900 + (month-14)/12) / 100)) / 4
     e += day - 32075
     return e
+}
+
+// Julian calendar until October 4, 1582
+// Algorithm from Frequently Asked Questions about Calendars by Claus Toendering
+func julianDayToJulianDate(jd int) (year, month, day int) {
+    jd += 32082
+    dd := (4*jd + 3) / 1461
+    ee := jd - (1461*dd)/4
+    mm := ((5 * ee) + 2) / 153
+    day = ee - (153*mm+2)/5 + 1
+    month = mm + 3 - 12*(mm/10)
+    year = dd - 4800 + (mm / 10)
+    if year <= 0 {
+        year--
+    }
+    return
+}
+
+// Gregorian calendar starting from October 15, 1582
+// This algorithm is from Henry F. Fliegel and Thomas C. Van Flandern
+func julianDayToGregorianDate(jd int) (year, month, day int) {
+    ell := uint64(jd) + 68569
+    n := (4 * ell) / 146097
+    ell = ell - (146097*n+3)/4
+    i := (4000 * (ell + 1)) / 1461001
+    ell = ell - (1461*i)/4 + 31
+    j := (80 * ell) / 2447
+    day = int(ell - (2447*j)/80)
+    ell = j / 11
+    month = int(j + 2 - (12 * ell))
+    year = int(100*(n-49) + i + ell)
+    return
 }
