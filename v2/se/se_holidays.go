bcd4284cd3b35a23e4edace0d47d138b9e9adb09
Ruben Das <ruben.das@atlar.com>
2023-12-14 16:18:15 +0100

    refactor(se_holidays): himmelsfärds dag -> himmelsfärdsdag
    This is one word: https://sv.wikipedia.org/wiki/Kristi_himmelsf%C3%A4rdsdag,
    which we already can see in the variable naming since it's not called
    KristiHimmelfardsDag.
    
    Also adding a missing S to the variable after Himmel.

--- v2/se/se_holidays.go
+++ v2/se/se_holidays.go
@@ -29,8 +29,8 @@ var (
 	// ForstaMaj represents Labour Day on 1-May
 	ForstaMaj = aa.WorkersDay.Clone(&cal.Holiday{Name: "Första maj", Type: cal.ObservancePublic})
 
-	// KristiHimmelfardsdag represents Ascension Day on the 39th day after Easter
-	KristiHimmelfardsdag = aa.AscensionDay.Clone(&cal.Holiday{Name: "Kristi himmelsfärds dag", Type: cal.ObservancePublic})
+	// KristiHimmelsfardsdag represents Ascension Day on the 39th day after Easter
+	KristiHimmelsfardsdag = aa.AscensionDay.Clone(&cal.Holiday{Name: "Kristi himmelsfärdsdag", Type: cal.ObservancePublic})
 
 	// Pingstdagen represents Pentecost Sunday on the 49th day after Easter
 	Pingstdagen = aa.Pentecost.Clone(&cal.Holiday{Name: "Pingstdagen", Type: cal.ObservancePublic})
@@ -108,7 +108,7 @@ var (
 		Langfredagen,
 		AnnandagPask,
 		ForstaMaj,
-		KristiHimmelfardsdag,
+		KristiHimmelsfardsdag,
 		Nationaldagen,
 		Midsommarafton,
 		Midsommardagen,
