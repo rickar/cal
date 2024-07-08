b3b76d684ed80f1df95c8e4bcca37c42c3707db7
Cyril Mouttet <cyril.mouttet@mst.ch>
2024-07-08 15:22:16 +0200

    Integrating luxembourg holidays

--- v2/lu/lu_holidays.go
+++ v2/lu/lu_holidays.go
@@ -1,6 +1,6 @@
 // (c) Rick Arnold. Licensed under the BSD license (see LICENSE).
 
-// Package be provides holiday definitions for Belgium.
+// Package be provides holiday definitions for Luxembourg.
 package be
 
 import (
@@ -11,53 +11,53 @@ import (
 )
 
 var (
-	// Nieuwjaar represents New Year's Day on 1-Jan
-	Nieuwjaar = aa.NewYear.Clone(&cal.Holiday{Name: "Nieuwjaarsdag", Type: cal.ObservancePublic})
+	// NeitJoer represents New Year's Day on 1-Jan
+	NeitJoer = aa.NewYear.Clone(&cal.Holiday{Name: "Neit Joer", Type: cal.ObservancePublic})
 
-	// Paasmaandag represents Easter Monday on the day after Easter
-	Paasmaandag = aa.EasterMonday.Clone(&cal.Holiday{Name: "Paasmaandag", Type: cal.ObservancePublic})
+	// Ouschterméindeg represents Easter Monday on the day after Easter
+	Ouschtermeindeg = aa.EasterMonday.Clone(&cal.Holiday{Name: "Ouschterméindeg", Type: cal.ObservancePublic})
 
-	// DagVanDeArbeid represents Labor Day on the first Monday in May
-	DagVanDeArbeid = aa.WorkersDay.Clone(&cal.Holiday{Name: "Dag van de Arbeid", Type: cal.ObservancePublic})
+	// DagVunAarbecht represents Labor Day on the first Monday in May
+	DagVunAarbecht = aa.WorkersDay.Clone(&cal.Holiday{Name: "Dag vun der Aarbecht", Type: cal.ObservancePublic})
 
-	// OnzeLieveHeerHemelvaart represents Ascension Day on the 39th day after Easter
-	OnzeLieveHeerHemelvaart = aa.AscensionDay.Clone(&cal.Holiday{Name: "Onze Lieve Heer Hemelvaart", Type: cal.ObservancePublic})
+	// ChristiHimmelfaart represents Ascension Day on the 39th day after Easter
+	ChristiHimmelfaart = aa.AscensionDay.Clone(&cal.Holiday{Name: "Christi Himmelfaart", Type: cal.ObservancePublic})
 
-	// Pinkstermaandag represents Pentecost Monday on the day after Pentecost (50 days after Easter)
-	Pinkstermaandag = aa.PentecostMonday.Clone(&cal.Holiday{Name: "Pinkstermaandag", Type: cal.ObservancePublic})
+	// Pengschtméindeg represents Pentecost Monday on the day after Pentecost (50 days after Easter)
+	Pengschtméindeg = aa.PentecostMonday.Clone(&cal.Holiday{Name: "Péngschtméindeg", Type: cal.ObservancePublic})
 
-	// NationaleFeestdag represents Belgian National Day on 21-Jul
-	NationaleFeestdag = &cal.Holiday{
-		Name:  "Nationale Feestdag",
+	// Nationalfeierdag represents Luxembourg National Day on 23-Jul
+	Nationalfeierdag = &cal.Holiday{
+		Name:  "Nationalfeierdag",
 		Type:  cal.ObservancePublic,
 		Month: time.July,
-		Day:   21,
+		Day:   23,
 		Func:  cal.CalcDayOfMonth,
 	}
 
-	// OnzeLieveVrouwHemelvaart represents Assumption of Mary on 15-Aug
-	OnzeLieveVrouwHemelvaart = aa.AssumptionOfMary.Clone(&cal.Holiday{Name: "Onze Lieve Vrouw Hemelvaart", Type: cal.ObservancePublic})
+	// MariesHimmelfaart represents Assumption of Mary on 15-Aug
+	MariesHimmelfaart = aa.AssumptionOfMary.Clone(&cal.Holiday{Name: "Maries Himmelfaart", Type: cal.ObservancePublic})
 
-	// Allerheiligen represents All Saints' Day on 1-Nov
-	Allerheiligen = aa.AllSaintsDay.Clone(&cal.Holiday{Name: "Allerheiligen", Type: cal.ObservancePublic})
+	// Allerhellgen represents All Saints' Day on 1-Nov
+	Allerhellgen = aa.AllSaintsDay.Clone(&cal.Holiday{Name: "Allerhellgen", Type: cal.ObservancePublic})
 
-	// Wapenstilstand represents Armistice Day on 11-Nov
-	Wapenstilstand = aa.ArmisticeDay.Clone(&cal.Holiday{Name: "Wapenstilstand", Type: cal.ObservancePublic})
+	// Chrëschtdag represents Christmas Day on 25-Dec
+	Chreschtdag = aa.ChristmasDay.Clone(&cal.Holiday{Name: "Chrëschtdag", Type: cal.ObservancePublic})
 
-	// Kerstmis represents Christmas Day on 25-Dec
-	Kerstmis = aa.ChristmasDay.Clone(&cal.Holiday{Name: "Kerstmis", Type: cal.ObservancePublic})
+	// ZweetenDagChrëschtdag represents second Christmas Day Day on 26-Dec
+	ZweetenDagChrëschtdag = aa.ChristmasDay2.Clone(&cal.Holiday{Name: "Zweeten Dag vum Chrëschtdag", Type: cal.ObservancePublic})
 
 	// Holidays provides a list of the standard national holidays
 	Holidays = []*cal.Holiday{
-		Nieuwjaar,
-		Paasmaandag,
-		DagVanDeArbeid,
-		OnzeLieveHeerHemelvaart,
-		Pinkstermaandag,
-		NationaleFeestdag,
-		OnzeLieveVrouwHemelvaart,
-		Allerheiligen,
-		Wapenstilstand,
-		Kerstmis,
+		NeitJoer,
+		Ouschtermeindeg,
+		DagVunAarbecht,
+		ChristiHimmelfaart,
+		Pengschtméindeg,
+		Nationalfeierdag,
+		MariesHimmelfaart,
+		Allerhellgen,
+		Chreschtdag,
+		ZweetenDagChrëschtdag,
 	}
 )
